5751453324e17a96ba099ca681e64b361badd6e6
anantamj <149796521+anantamj@users.noreply.github.com>
2024-08-02 20:28:16 +0530

    Update main-ping.js

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
     key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' },
     message: {
       contactMessage: {
-        displayName: `Shizo The Techie`,
+        displayName: `Eren Yeager`,
         vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:'GURU-BOT'\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
       },
     },
